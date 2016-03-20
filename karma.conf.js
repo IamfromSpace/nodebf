0cab2a58fcf6f7eabb96109c93dc48e18ecd9096
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2016-03-20 12:42:12 -0700

    Remove unused config

--- karma.conf.js
+++ karma.conf.js
@@ -11,9 +11,7 @@ module.exports = function(config) {
     plugins: [
       'karma-mocha',
       'karma-firefox-launcher',
-      'karma-common-js',
       'karma-webpack',
-      require('./karma-plugin.js')
     ],
 
 
