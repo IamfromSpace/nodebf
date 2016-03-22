6865e3995428794330fdf172396cc5facdcb4fd2
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2016-03-21 18:13:04 -0700

    Add a test for ignoring comment loops.

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,8 +1,16 @@
 var helloWorld = require('./fixtures/helloworld.bf');
+var helloWorldWithCommentLoop = require('./fixtures/helloworldWithCommentLoop.bf');
 
 
 describe('Compiler', function() {
   it('should compile helloworld.bf correctly', function() {
+    var result = helloWorld();
+    if (result !== 'Hello World!\n') {
+      throw new Error('didn\'t match! Got: ' + result);
+    }
+  });
+
+  it('should compile files with comment loops correctly', function() {
     var result = helloWorld();
     if (result !== 'Hello World!\n') {
       throw new Error('didn\'t match! Got: ' + result);
